3b141a9d8f91b98594aef17828243c2a28ec0cae
priti2402 <priti.kokare@cumminscollege.in>
2023-07-19 22:28:03 +0530

    Angular

--- src/app/pages/scan/scan.component.ts
+++ src/app/pages/scan/scan.component.ts
@@ -1,23 +1,41 @@
 import { Component,OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import{EmpService}from './EmpService';
+//import { HttpClient,HttpHeaders } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//import{EmpService}from './EmpService';
 
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.css']
 })
-export class  ScanComponent implements OnInit {
+export class  ScanComponent  {
+  selecttedFile:File |null=null;
   constructor(private http: HttpClient){ 
+    onFileChange(event: any) {
+      this.selectedFile = event.target.files[0];
 
   }
-  ngOnInit():void {
-  
+  onFileSelected(event: Event) {
+    event.preventDefault();
 
-  }
-  onChangef():void {
-    this.http.post('',)
-  }
+    if (!this.selectedFile) {
+      console.log('No file selected');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
+
+    this.http.post('https://api.example.com/upload', formData)
+      .subscribe(response => {
+        console.log('File uploaded successfully');
+      }, error => {
+        console.error('File upload failed:', error);
+      });
+    }
+ 
 }
 
 
